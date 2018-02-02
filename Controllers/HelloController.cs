6435ef70e240082a4dd6e2a6374dc8a218abb25c
srikanthpragada <srikanthpragada@gmail.com>
2018-02-02 09:36:27 +0530

    2-FEB

---
+++ Controllers/HelloController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace mvcdemo.Controllers
+{
+    public class HelloController : Controller
+    {
+        // GET: Hello  or Hello/Index 
+        public ActionResult Index()
+        {
+            ViewBag.Title = "Srikanth Technologies";
+            return View();
+        }
+
+        //  Hello/About 
+        public ActionResult About()
+        {
+            return View();
+        }
+    }
+}
