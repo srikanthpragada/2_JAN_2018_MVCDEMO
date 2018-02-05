b79b2140b53aefff79eebd5d4916d7ad0288e6b3
srikanthpragada <srikanthpragada@gmail.com>
2018-02-05 09:35:01 +0530

    5-FEB

---
+++ Controllers/BankController.cs
@@ -0,0 +1,34 @@
+﻿using mvcdemo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace mvcdemo.Controllers
+{
+    public class BankController : Controller
+    {
+        // GET: Bank
+        public ActionResult Interest()
+        {
+            var model = new InterestViewModel();
+            model.Rate = 12.5;
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult Interest(InterestViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                 model.Interest = model.Amount * model.Rate / 100;
+            }
+
+            return View(model);
+        }
+
+
+
+    }
+}
---
+++ Models/InterestViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace mvcdemo.Models
+{
+    public class InterestViewModel
+    {
+        [Range(1000, Double.MaxValue, ErrorMessage ="Minimum value must be 1000")]
+        public double Amount { get; set; }
+
+        [Range(5,50, ErrorMessage ="Valid interest rate is between 5 and 50")]
+        public double Rate { get; set; }
+
+        public double Interest { get; set; }
+    }
+}
