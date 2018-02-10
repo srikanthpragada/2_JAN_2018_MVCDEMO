1f69d350d4abc182ec4b19f4a8494c47d2d36ba5
srikanthpragada <srikanthpragada@gmail.com>
2018-02-10 09:32:45 +0530

    10-FEB

---
+++ Controllers/MailController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Web;
+using System.Web.Mvc;
+
+namespace mvcdemo.Controllers
+{
+    public class MailController : Controller
+    {
+        
+        public ActionResult Send()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateInput(false)]
+        public ActionResult Send(string subject, string body)
+        {
+            try
+            {
+                MailMessage m = new MailMessage();
+                m.To.Add(new MailAddress("a@demo.com"));
+                m.From = new MailAddress("b@demo.com");
+                m.Subject = subject;
+                m.IsBodyHtml = true;
+                m.Body = body;
+
+
+                //add web.config as attachment
+
+                Attachment a1 = new Attachment(Request.MapPath("~/web.config"));
+                m.Attachments.Add(a1);
+
+                SmtpClient smtp = new SmtpClient("localhost", 25);
+                // logon to server 
+                smtp.UseDefaultCredentials = false;
+                smtp.EnableSsl = false;
+                smtp.Credentials = new System.Net.NetworkCredential("b@demo.com", "b");
+
+                smtp.Send(m);
+                ViewBag.Message = "Mail sent successfully!";
+            }
+            catch (Exception ex)
+            {
+                ViewBag.Message = "Error --> " + ex.Message;
+            }
+            return View();
+        }
+    }
+}
--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -3,22 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
 
 namespace mvcdemo.Controllers
 {
     public class UserController : Controller
     {
-        
+        [HttpGet]
         public ActionResult Login()
         {
+            Session.Add("ReturnUrl", Request.QueryString["ReturnUrl"]);
             return View();
+
         }
 
         [HttpPost]
         public ActionResult Login(string username, string password)
         {
             if (username == "admin" && password == "admin")
-                return RedirectToAction(Request.QueryString["ReturnUrl"]);
+            {
+                FormsAuthentication.SetAuthCookie("admin", false);
+                return  RedirectToAction(".." + Session["ReturnUrl"].ToString ()); 
+            }
             else
                 ViewBag.Message = "Sorry! Invalid Login!";
 
