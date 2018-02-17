a931b7175716f1ee62a14d497329efae8d9b7d50
srikanthpragada <srikanthpragada@gmail.com>
2018-02-17 09:40:16 +0530

    17-FEB

---
+++ Controllers/CategoryAPIController.cs
@@ -0,0 +1,66 @@
+﻿using mvcdemo.Models.Ef;
+using System.Data.Entity;
+using System;
+using System.Collections.Generic;
+using System.Web.Http;
+using System.Linq;
+
+namespace mvcdemo.Controllers
+{
+    public class CategoryAPIController : ApiController
+    {
+
+        //  Get CategoryAPI
+        public  IEnumerable<Category>  Get()
+        {
+            var ctx = new InventoryContext();
+            return ctx.Categories.ToList<Category>();
+
+        }
+        // Get CategoryAPI/id
+        public Category Get(string id)
+        {
+            var ctx = new InventoryContext();
+            var cat = ctx.Categories.Find(id);
+            if (cat == null)
+                throw new HttpResponseException(System.Net.HttpStatusCode.NotFound);  // 404
+
+            return cat;
+        }
+
+        // Delete   CategoryAPI/id
+        [HttpDelete]
+        public void Delete(string id)
+        {
+            var ctx = new InventoryContext();
+            var cat = ctx.Categories.Find(id);
+            if (cat == null)
+                throw new HttpResponseException(System.Net.HttpStatusCode.NotFound);  // 404
+
+            ctx.Categories.Remove(cat);
+            ctx.SaveChanges();
+        }
+
+        // Post   CategoryAPI
+        [HttpPost]
+        public void Post(Category cat)
+        {
+            var ctx = new InventoryContext();
+            ctx.Categories.Add(cat);
+            ctx.SaveChanges();
+        }
+
+        // Put   CategoryAPI/id
+        [HttpPut]
+        public void Put(string id, Category cat)
+        {
+            var ctx = new InventoryContext();
+            var dbcat = ctx.Categories.Find(id);
+            if (dbcat == null)
+                throw new HttpResponseException(System.Net.HttpStatusCode.NotFound);  // 404
+
+            dbcat.Description = cat.Description;
+            ctx.SaveChanges();
+        }
+    }
+}
---
+++ Controllers/GreetController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace mvcdemo.Controllers
+{
+    public class GreetController : ApiController
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "Hello Web API";
+        }
+        [HttpGet]
+        public string Get(string id)
+        {
+            return id + ",Hello Web API";
+        }
+    }
+}
